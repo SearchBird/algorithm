aa60825a44b156ff8d79efb21a83373d70f5165a
谭钜文 <3371781537@qq.com>
2022-03-15 18:00:39 +0800

    edt

--- src/org/tjw/leetcode/algorithm/Test.java
+++ src/org/tjw/leetcode/algorithm/Test.java
@@ -1,44 +1,41 @@
 package org.tjw.leetcode.algorithm;
 
-import java.util.Map;
-import java.util.TreeMap;
+import org.tjw.leetcode.algorithm.linkedList.ListNode;
+
+import java.util.*;
 
 public class Test {
 
-    String str = new String("good");
-    char[] ch = {'a','b','c'};
     public static void main(String[] args) {
-        TreeMap<Integer,Integer> map = new TreeMap<>();
-            Integer e = map.firstKey();
+        String str = "11";
+        String str2 = new String("11");
+        String str3 = new String("11");
+        System.out.println(str == str2);
+        System.out.println(str3 == str2);
     }
 
-    public void change(String str, char ch[]){
-        double d = 1.1e1;
-    }
-
-    private static int[][] stack = new int[2][1000];
-    class Solution {
-        public String countAndSay(int n) {
-            int pre = 1, cur = 0, idx1 = 0, idx2 = 0;
-            stack[cur][idx1 ++] = 1;
-            while(n -- > 1) {
-                for(int i = 0, len = idx1; i < idx1;) {
-                    int tmp = stack[cur][i], cnt = 0;
-                    while(stack[cur][i] == tmp) {
-                        i ++; cnt ++;
+    static class Solution {
+        public boolean canPlaceFlowers(int[] flowerbed, int n) {
+            for(int i = 0, len = flowerbed.length; i < len; i ++)
+                if(flowerbed[i] == 0) {
+                    boolean left = true;
+                    if(i - 1 >= 0) left = flowerbed[i - 1] != 1;
+                    boolean right = true;
+                    if(i + 1 < len) right = flowerbed[i + 1] != 1;
+                    if(left && right) {
+                        if(n <= 0) return true;
+                        flowerbed[i] = 1;
+                        n --;
                     }
-                    stack[pre][idx2 ++] = cnt;
-                    stack[pre][idx2 ++] = tmp;
+
                 }
-                pre ^= 1;
-                cur ^= 1;
-                idx1 = idx2;
-                idx2 = 0;
-            }
-            StringBuilder builder = new StringBuilder();
-            for(int i = 0; i < idx1; i ++) builder.append(stack[cur][i]);
-            return builder.toString();
+            return n == 0;
         }
     }
 
 }
+
+/**
+ * P Y A I H
+ * A P L S
+ */
